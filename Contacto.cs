037a58f03e57681a4922c21ee3d8661237aaa5ea
emavb <emavb98@gmail.com>
2025-10-11 18:23:04 -0600

    Agregada clase Contacto

---
+++ Contacto.cs
@@ -0,0 +1,14 @@
+﻿namespace GestorContactos
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public Contacto(string nombre, string telefono)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+        }
+    }
+}
